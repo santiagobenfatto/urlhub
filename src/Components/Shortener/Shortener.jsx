6cde8af81fac099fda685e32b9b1b8063b710933
santiagobenfatto <santiagobenfatto@outlook.com>
2024-10-26 20:31:05 -0300

    Rollup options updated

--- src/Components/Shortener/Shortener.jsx
+++ src/Components/Shortener/Shortener.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Box, Button, TextField, Typography } from '@mui/material'
 import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh'
 
