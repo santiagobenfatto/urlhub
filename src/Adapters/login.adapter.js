f2e14abc31acc0475c75e7fc64d80759a282de6c
santi <santiagobenfatto@outlook.com>
2025-08-27 13:24:50 -0300

    l

--- src/Adapters/login.adapter.js
+++ src/Adapters/login.adapter.js
@@ -1,10 +1,8 @@
 export const loginAdapter = async (userData) => {
     return {
-        id: userData?._id || '',
-        userName: userData?.name || '',
+        userName: userData?.first_name || '',
+        nickname: userData?.nickname || '',
         email: userData?.email || '',
-        linkList: userData?.link_list || [],
-        hubSetup: userData?.hub_setup || null,
-        response: userData?.response || {}
+        hubSetup: userData?.hub_setup || null
     }
 }
